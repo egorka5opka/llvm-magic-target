575ef46618409284a1de383b275292d6a1de97fd
egorka5opka <egortuisov@gmail.com>
2026-03-24 23:50:49 +0300

    [Magic] 15

--- llvm/lib/Target/Magic/MagicISelLowering.cpp
+++ llvm/lib/Target/Magic/MagicISelLowering.cpp
@@ -39,6 +39,27 @@ MagicTargetLowering::MagicTargetLowering(const TargetMachine &TM,
     : TargetLowering(TM), STI(STI) {
   MAGIC_DUMP_RED
   addRegisterClass(MVT::i32, &Magic::GPRRegClass);
+  computeRegisterProperties(STI.getRegisterInfo());
+
+  setStackPointerRegisterToSaveRestore(Magic::R1);
+
+  // setSchedulingPreference(Sched::Source);
+
+  for (unsigned Opc = 0; Opc < ISD::BUILTIN_OP_END; ++Opc)
+    setOperationAction(Opc, MVT::i32, Expand);
+
+  setOperationAction(ISD::ADD, MVT::i32, Legal);
+  setOperationAction(ISD::MUL, MVT::i32, Legal);
+  // ...
+  setOperationAction(ISD::LOAD, MVT::i32, Legal);
+  setOperationAction(ISD::STORE, MVT::i32, Legal);
+
+  setOperationAction(ISD::Constant, MVT::i32, Legal);
+  setOperationAction(ISD::UNDEF, MVT::i32, Legal);
+
+  setOperationAction(ISD::BR_CC, MVT::i32, Custom);
+
+  setOperationAction(ISD::FRAMEADDR, MVT::i32, Legal);
 }
 
 const char *MagicTargetLowering::getTargetNodeName(unsigned Opcode) const {
