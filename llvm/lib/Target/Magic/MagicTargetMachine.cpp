f0d91818aa9789a9b54f5a31faebbb89233749b2
egorka5opka <egortuisov@gmail.com>
2026-03-24 23:00:18 +0300

    [Magic] 10

--- llvm/lib/Target/Magic/MagicTargetMachine.cpp
+++ llvm/lib/Target/Magic/MagicTargetMachine.cpp
@@ -1,6 +1,7 @@
 #include "MagicTargetMachine.h"
 #include "Magic.h"
 #include "TargetInfo/MagicTargetInfo.h"
+#include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/MC/TargetRegistry.h"
 #include <optional>
@@ -19,9 +20,10 @@ MagicTargetMachine::MagicTargetMachine(const Target &T, const Triple &TT,
                                        std::optional<Reloc::Model> RM,
                                        std::optional<CodeModel::Model> CM,
                                        CodeGenOptLevel OL, bool JIT)
-    : CodeGenTargetMachineImpl(
-          T, "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32", TT, CPU, FS, Options,
-          Reloc::Static, getEffectiveCodeModel(CM, CodeModel::Small), OL) {
+    : CodeGenTargetMachineImpl(T, "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32",
+                               TT, CPU, FS, Options, Reloc::Static,
+                               getEffectiveCodeModel(CM, CodeModel::Small), OL),
+      TLOF(std::make_unique<TargetLoweringObjectFileELF>()) {
   MAGIC_DUMP_CYAN
   initAsmInfo();
 }
@@ -50,4 +52,10 @@ public:
 TargetPassConfig *MagicTargetMachine::createPassConfig(PassManagerBase &PM) {
   MAGIC_DUMP_CYAN
   return new MagicPassConfig(*this, PM);
+}
+
+
+TargetLoweringObjectFile *SimTargetMachine::getObjFileLowering() const {
+  MAGIC_DUMP_CYAN
+  return TLOF.get();
 }
--- llvm/lib/Target/Magic/MagicTargetMachine.h
+++ llvm/lib/Target/Magic/MagicTargetMachine.h
@@ -8,6 +8,8 @@ namespace llvm {
 extern Target TheMagicTarget;
 
 class MagicTargetMachine : public CodeGenTargetMachineImpl {
+  std::unique_ptr<TargetLoweringObjectFile> TLOF;
+
 public:
   MagicTargetMachine(const Target &T, const Triple &TT, StringRef CPU,
                    StringRef FS, const TargetOptions &Options,
@@ -17,6 +19,7 @@ public:
                    
   // Pass Pipeline Configuration
   TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+  TargetLoweringObjectFile *getObjFileLowering() const override;
 };
 } // end namespace llvm
 
